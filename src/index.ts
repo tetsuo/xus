b0a8f741d06ee009e33bd1355df39c39ef9acc26
tetsuo <ogunduz@gmail.com>
2017-07-04 11:34:25 +0200

    Normalize to React props & apply some rules
    - Self closing tags cannot have children
    - value -> defaultValue
    - for -> htmlFor
    - class -> className
    - Textarea cannot have children if it has default value
    
    See more rules here: https://github.com/yannickcr/eslint-plugin-react/tree/master/docs/rules

--- src/index.ts
+++ src/index.ts
@@ -21,31 +21,95 @@ export type ObserverComponentProps = {
     traverseFn?: (children: (React.ReactNode | string)[] | null, node: ParseTree | string, top?: any[]) => React.ReactNode[]
 }
 
+const selfClosingTags = [
+    "area",
+    "base",
+    "br",
+    "col",
+    "embed",
+    "hr",
+    "img",
+    "input",
+    "keygen",
+    "link",
+    "menuitem", // see: https://github.com/facebook/react/blob/85dcbf83/src/renderers/dom/shared/ReactDOMComponent.js#L437
+    "meta",
+    "param",
+    "source",
+    "track",
+    "wbr"
+]
+
 function createObserverComponent(options: XusOptions<any>, props: ObserverComponentProps) {
-    const {
-        React
-    } = options
+    const { React } = options
+    const { parseTree, traverseFn, state } = props
 
-    const {
-        parseTree,
-        traverseFn,
-        state
-    } = props
+    const type = parseTree[ParseTreeIndex.Tag] as any
 
-    let actualProps = parseTree[ParseTreeIndex.Attrs] as { [s: string]: any } | null
+    let children: React.ReactNode[] = null
 
-    if (typeof actualProps === "object") {
-        actualProps = actualProps.attributes
+    let visitChildren = (selfClosingTags.indexOf(type) === -1)
+
+    let attrs = parseTree[ParseTreeIndex.Attrs] as { [s: string]: any } | null
+
+    if (attrs === null || typeof attrs !== "object") {
+        attrs = {}
     }
 
-    return React.createElement(
-        parseTree[ParseTreeIndex.Tag] as any,
-        actualProps,
-        (parseTree[ParseTreeIndex.Children] as (ParseTree | string)[])
-            .reduce<React.ReactNode>((acc: (React.ReactNode | string)[], childTree: (ParseTree | string)) => {
+    const normalizedProps = Object.keys(attrs).reduce((acc: any, key) => {
+        key = key.toLowerCase()
+
+        let value: any = attrs[key]
+
+        if (typeof type === "string" && (type === "input" || type === "textarea")) {
+            if (attrs.hasOwnProperty("checked")) {
+                acc.defaultChecked = attrs.checked
+            }
+            if (attrs.hasOwnProperty("value")) {
+                acc.defaultValue = attrs.value
+                if (type === "textarea") {
+                    visitChildren = false
+                }
+            }
+        }
+
+        if (key === "class") {
+            acc.className = value
+        } else if (key === "for") {
+            acc.htmlFor = value
+        }
+
+        return acc
+    }, {}) as any
+
+
+    if (visitChildren) {
+        children = (parseTree[ParseTreeIndex.Children] as (ParseTree | string)[])
+            .reduce<React.ReactNode[]>((acc: (React.ReactNode | string)[], childTree: (ParseTree | string)) => {
                 return traverseFn(acc, childTree, [ state ])
             }, [])
-    )
+    }
+
+
+
+
+
+
+
+    // if (attrs.dataset) {
+
+    // }
+
+    // if (properties.dataset) {
+    //     Object.keys(properties.dataset).forEach(function unnest(attrName) {
+    //     var dashedAttr = attrName.replace(/([a-z])([A-Z])/, function dash(match) {
+    //         return match[0] + '-' + match[1].toLowerCase();
+    //     });
+    //     properties['data-' + dashedAttr] = properties.dataset[attrName];
+    //     });
+    // }
+
+    return React.createElement(type, normalizedProps, children)
 }
 
 export function xus<T>(html: string, state: any, options: XusOptions<T>, cb: (buildError, node?: T, html?: string) => void) {
@@ -92,12 +156,9 @@ export function xus<T>(html: string, state: any, options: XusOptions<T>, cb: (bu
         return React.createElement(
             registry.ObserverComponent,
             {
-                ...{
-                    parseTree: props.parseTree,
-                    traverseFn: props.traverseFn,
-                    state: props.state[props.state.length - 1]
-                },
-                ...props.attributes
+                parseTree: props.parseTree,
+                traverseFn: props.traverseFn,
+                state: props.state[props.state.length - 1]
             },
             children)
     }
