27d13d00fc3bc843effcea72f57b2142835f6585
tetsuo <ogunduz@gmail.com>
2017-07-08 19:27:55 +0200

    Tokenize attributes

--- src/test/lexer.ts
+++ src/test/lexer.ts
@@ -12,7 +12,7 @@ test("tokenize html", t => {
 </table>`
 
     const expected = [
-        [ 1, "table", { cols: "3" } ],
+        [ 1, "table", { cols: [ [ 3, "3" ] ] } ],
         [ 3, "\n  " ],
         [ 1, "tbody", {} ],
         [ 3, "blah blah blah" ],
@@ -31,7 +31,7 @@ test("tokenize html", t => {
         [ 2, "tr" ],
         [ 3, "\n  " ],
         [ 1, "tr", {} ],
-        [ 1, "td", { bgcolor: "blue" } ],
+        [ 1, "td", { bgcolor: [ [ 3, "blue" ] ] } ],
         [ 3, "is" ],
         [ 2, "td" ],
         [ 2, "tr" ],
@@ -61,18 +61,92 @@ test("tokenize stache", t => {
 </table>`
 
     const expected = [
-        [ 1, "table", { cols: "3" } ],
+        [ 1, "table", { cols: [ [ 3, "3" ] ] } ],
         [ 3, "\n  " ],
         [ 5, "fruits" ],
         [ 3, "\n    " ],
         [ 1, "tr", {} ],
         [ 3, "\n      " ],
-        [ 1, "td", { bgcolor: "blue" } ],
+        [ 1, "td", { bgcolor: [ [ 3, "blue" ] ] } ],
         [ 4, "name" ],
         [ 2, "td" ],
         [ 3, "\n      " ],
         [ 5, "proteins" ],
         [ 1, "td", {} ],
+        [ 4, "name" ],
+        [ 2, "td" ],
+        [ 6, "proteins" ],
+        [ 3, "\n    " ],
+        [ 2, "tr" ],
+        [ 3, "\n  " ],
+        [ 6, "fruits" ],
+        [ 3, "\n" ],
+        [ 2, "table" ]
+    ]
+
+    t.plan(expected.length)
+
+    util.fromString(html)
+        .pipe(lexer.tokenize())
+        .on("data", data => {
+            const x = expected.shift()
+            t.deepEqual(data, x)
+        })
+})
+
+test("tokenize attrs", t => {
+    const html =
+`<table cols={numcols}>
+  {#fruits}
+    <tr bgcolor="{#isx}{xthing}{/isx}">
+      <td bgcolor="{bgcolor}">{name}</td>
+      {#proteins}<td class='{foo} {bar} xxx'>{name}</td>{/proteins}
+    </tr>
+  {/fruits}
+</table>`
+
+    const expected = [
+        [ 1, "table",
+            {
+                cols: [
+                    [ 4, "numcols" ]
+                ]
+            }
+        ],
+        [ 3, "\n  " ],
+        [ 5, "fruits" ],
+        [ 3, "\n    " ],
+        [ 1, "tr",
+            {
+                bgcolor: [
+                    [ 5, "isx" ],
+                    [ 4, "xthing" ],
+                    [ 6, "isx" ]
+                ]
+            }
+        ],
+        [ 3, "\n      " ],
+        [ 1, "td",
+            {
+                bgcolor: [
+                    [ 4, "bgcolor" ]
+                ]
+            }
+        ],
+        [ 4, "name" ],
+        [ 2, "td" ],
+        [ 3, "\n      " ],
+        [ 5, "proteins" ],
+        [ 1, "td",
+            {
+                class: [
+                    [ 4, "foo" ],
+                    [ 3, " " ],
+                    [ 4, "bar" ],
+                    [ 3, " xxx"]
+                ]
+            }
+        ],
         [ 4, "name" ],
         [ 2, "td" ],
         [ 6, "proteins" ],
